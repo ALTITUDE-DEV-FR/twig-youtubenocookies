89c451cf0816288ac76ee0d7163ea254e68549d5
ALTITUDE DEV <contact@altitude-dev.fr>
2024-07-20 11:39:11 +0200

    Create LazyContentExtension.php

---
+++ LazyContentExtension.php
@@ -0,0 +1,60 @@
+<?php 
+/**
+ * Twig LazyContent escape cookies and lazyload
+ * use lazy_content
+ */
+namespace App\ThirdParty\TwigExtensions;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class LazyContentExtension extends AbstractExtension
+{
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('lazy_content', [$this, 'LazyContent'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function LazyContent($content)
+    {
+
+        $dom = new \DOMDocument('1.0', 'UTF-8');
+        libxml_use_internal_errors(true);
+        $dom->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        libxml_clear_errors();
+
+        // Iframes src to nocookies + lazyload
+        $iframes = $dom->getElementsByTagName('iframe');
+        foreach ($iframes as $iframe) {
+            $src = $iframe->getAttribute('src');
+            if (strpos($src, '//www.youtube.com') !== false) {
+                $newSrc = str_replace('//www.youtube.com/', '//www.youtube-nocookie.com/', $src);
+            } elseif (strpos($src, '//youtube.com') !== false) {
+                $newSrc = str_replace('//youtube.com/', '//www.youtube-nocookie.com/', $src);
+            } else {
+                continue;
+            }
+            $iframe->setAttribute('data-src', $newSrc);
+            $iframe->removeAttribute('src');
+            $iframe->setAttribute('class', trim($iframe->getAttribute('class') . ' lazy'));
+        }
+
+        // images src to data-src + lazyload
+        $images = $dom->getElementsByTagName('img');
+        foreach ($images as $img) {
+            $src = $img->getAttribute('src');
+            if ($src) {
+                $img->setAttribute('data-src', $src);
+                $img->removeAttribute('src');
+                $img->setAttribute('class', trim($img->getAttribute('class') . ' lazyload androModal'));
+            }
+        }
+
+        return $dom->saveHTML();
+    }
+    
+}
